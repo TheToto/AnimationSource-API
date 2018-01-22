57f8b5b44d06b344fe3149c66b42658045444ebd
Thomas <thomas.lupin@epita.fr>
2018-01-22 17:10:42 +0100

    test

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ router.route('/')
       methode : req.method});
 });
 
+router.route('/test')
+//POST
+.post(function(req,res){
+  testConnect(req,res);
+})
+
 router.route('/profile/:id')
 // GET
 .get(function(req,res){ 
@@ -506,6 +512,56 @@ function sendCommentProfile(req, res) {
       //res.send(body);
       res.json({
         confirm : true,
+        options : options, 
+        methode : req.method
+      });
+      console.log("ok");
+    } else { 
+      res.json({
+        error : "error request"
+      });
+    }
+  });
+}
+
+function testConnect(req, res) {
+
+  var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded',
+    'cookie' : req.body.cookie
+  }
+
+  var options = {
+    url: 'https://www.animationsource.org/hub/fr/profile/',
+    method: 'POST',
+    encoding: 'binary',
+    headers: headers,
+    form: {
+      
+    }
+  }
+  
+  request(options, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+      let success = false;
+      let infos = {}
+      var reg = /&numg=([0-9]+)/
+      if ($('.emsg').length == 0) {
+        success = true;
+        infos = {
+          avatar: $('#avatar').val(),
+          pseudo:  $('.bcentre big b').text().slice(1),
+          id: reg.exec($('#previewprofile').attr('href'))[1]
+        }
+      }
+
+
+      res.json({
+        success: success,
+        infos: infos,
         options : options, 
         methode : req.method
       });
