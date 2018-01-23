a599f4054714a9d3ad7441140ab229b633f9e1d3
Thomas <thomas.lupin@epita.fr>
2018-01-23 01:31:06 +0100

    Search engine + mp  added.

--- index.js
+++ index.js
@@ -27,7 +27,19 @@ router.route('/')
 router.route('/test')
 //POST
 .post(function(req,res){
-  testConnect(req,res);
+  testCotestnnect(req,res);
+})
+
+router.route('/mp')
+//POST
+.post(function(req,res){
+  getMp(req,res);
+})
+
+router.route('/mp/:id')
+//POST
+.post(function(req,res){
+  readMp(req,res);
 })
 
 router.route('/profile/:id')
@@ -66,6 +78,12 @@ router.route('/chat/:lang/:sitename/:chat')
 .post(function(req,res){
   sendAsChat(req,res);
 });
+
+router.route('/search/:lang/:sitename/:type/:subtype/:search')
+// GET
+.get(function(req,res){ 
+    search(req,res);
+})
 
 app.use(router);
 
@@ -524,7 +542,7 @@ function sendCommentProfile(req, res) {
   });
 }
 
-function testConnect(req, res) {
+function test(req, res) {
 
   var headers = {
     'User-Agent':       'Super Agent/0.0.1',
@@ -562,6 +580,173 @@ function testConnect(req, res) {
       res.json({
         success: success,
         infos: infos,
+        options : options, 
+        methode : req.method
+      });
+      console.log("ok");
+    } else { 
+      res.json({
+        error : "error request"
+      });
+    }
+  });
+}
+
+function getMp(req, res) {
+  var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded',
+    'cookie' : req.body.cookie
+  }
+
+  var options = {
+    url: 'https://www.animationsource.org/hub/en/readmp/',
+    method: 'GET',
+    encoding: 'binary',
+    headers: headers,
+    form: { }
+  }
+  
+  request(options, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+  
+      let result =[];
+      $('form .bs2 tbody').children().each(function(i, elem) {
+        let temp = $(this).children().first();
+        let m_id = temp.children().first().val();
+        temp = temp.next().next();
+        let m_text = temp.find('span').text();
+        temp = temp.next();
+        let m_from = temp.text();
+        temp = temp.next();
+        let m_date = temp.find('span').text();
+        result[i] = {
+          id: m_id,
+          title: m_text,
+          from: m_from,
+          date: m_date
+        }
+      });
+      result.shift();
+      result.pop();
+      result.pop();
+  
+
+      res.json({
+        count: result.length,
+        mps: result,
+        options : options, 
+        methode : req.method
+      });
+      console.log("ok");
+    } else { 
+      res.json({
+        error : "error request"
+      });
+    }
+  });
+}
+
+function readMp(req, res) { // NEED TO CLEAN THIS FUNC. NEED BETTER RESPONCE
+  var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded',
+    'cookie' : req.body.cookie
+  }
+
+  var options = {
+    url: 'https://www.animationsource.org/hub/en/readmp/&read=' + req.params.id,
+    method: 'GET',
+    encoding: 'binary',
+    headers: headers,
+    form: { }
+  }
+  
+  request(options, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+      let reg = /&to=([0-9]+)&/;
+      let reg2 = /From :(.*?)Date :(.*?)Subject :(.*?)Message :(.*?)<hr>/;
+      let removeHTML = /<(?:.|\n)*?>/gm
+
+      let form = reg.exec($('form[name=pmsend]').attr('action'))[1];
+      let content = $('.content_important').parent().html();
+
+      res.json({
+        id : req.params.id,
+        content: content,
+        author_id: form,
+        options : options, 
+        methode : req.method
+      });
+      console.log("ok");
+    } else { 
+      res.json({
+        error : "error request"
+      });
+    }
+  });
+}
+
+function search(req, res) { // NEED TO CLEAN THIS FUNC. NEED BETTER RESPONCE
+  var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded',
+   // 'cookie' : req.body.cookie
+  }
+
+  var options = {
+    url: 'https://www.animationsource.org/' + req.params.sitename + '/' + req.params.lang + '/cherche/&searchtext=' + req.params.search + '&searchtype=' + req.params.type + '&subtype=' + req.params.subtype +'&nb=999999',
+    method: 'GET',
+    encoding: 'binary',
+    headers: headers,
+    form: { }
+  }
+  
+  request(options, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+      if ($('.emsg').length > 0) {
+        res.json({
+          success: false,
+          type: req.params.type,
+          subtype: req.params.subtype,
+          result: [],
+          options : options, 
+          methode : req.method
+        });
+        return;
+      }
+      let reg = /\/([0-9]+).html/
+      let regdetails = /&detail=([0-9]+)/
+      let list = $('ul.f10');
+      let result = [];
+      list.children().each(function(i, elem) {
+        let link = $(this).find('a').attr('href');
+        let id, detail;
+        try {
+          id = reg.exec(link)[1];
+        } catch (e) {}
+        try {
+          detail = regdetails.exec(link)[1];
+        } catch (e) {}
+
+        result[i] = {
+          id: id,
+          detail: detail,
+          content: $(this).find('span').text(),
+
+        }
+      });
+
+      res.json({
+        success: true,
+        type: req.params.type,
+        result: result,
         options : options, 
         methode : req.method
       });
