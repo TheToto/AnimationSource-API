0de9e1af2c997e3ca9125b8200ef9f565c3773f2
Thomas <thomas.lupin@epita.fr>
2018-02-25 05:30:26 +0100

    database modif

--- notif.js
+++ notif.js
@@ -18,7 +18,7 @@ var config = {
 firebase.initializeApp(config);
 
 module.exports.register = function(req,res) {
-  var reg = /\[[A-Za-z]*\]/;
+  var reg = /\[[A-Za-z0-9]*\]/;
 
   var newUserKey = firebase.database().ref().child('users').push().key;
   console.log(req.body);
